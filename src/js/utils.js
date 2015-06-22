c7576f74609e741a2d0cd7b7fe7db318ee5b3984
David Zhu <hdavidzhu@gmail.com>
2015-06-22 18:03:27 -0400

    Weird how these files aren't added.

---
+++ src/js/utils.js
@@ -0,0 +1,11 @@
+module.exports = {
+  pushIfUnique: function(currentArray, queuedItem) {
+    var found = $.inArray(queuedItem, currentArray);
+    if (found >= 0) {
+      return;
+    } else {
+      // Element was not found, add it.
+      currentArray.push(queuedItem);
+    }
+  }
+}
